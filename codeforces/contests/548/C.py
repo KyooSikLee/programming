dbed41198e07868096da38749e35c59a76d59db8
KyooSik Lee <kyoosik.lee@gmail.com>
2019-04-06 13:01:53 +0900

    Add 550

--- codeforces/contests/548/C.py
+++ codeforces/contests/548/C.py
@@ -1,32 +1,30 @@
-nk = input().split(" ")
+def Cluster(adjList, visited):
+
+nk =  input().split(" ")
 n = int(nk[0])
 k = int(nk[1])
-redList = [False]*n
 
-iter = 0
-cache = {}
+adjList = [[] for i in range(n+1)]
+visited = [False]*(n+1)
+lookupList = []
+iteration = 0
 for i in range(n-1):
-    appended = False
     l = input().split(" ")
     l0 = int(l[0])
     l1 = int(l[1])
     l2 = int(l[2])
     if l2 == 0:
-        if redList[l0] == True and redList[l1] == True:
-            pass
-        elif redList[l0] == True:
-            
-        elif redList[l1] == True:
-            
-        else:
-            cache[iter] = [l0, l1]
-            iter += 1
+        adjList[l0].append(l1)
+        adjList[l1].append(l0)
+        lookupList.append(l0)
+        lookupList.append(l1)
 
-minus = 0
-for c in cache:
-    p = (len(cache[c]))
-    minus += ((p**k)- p)
-print((n**k-n-minus) % (10**9 + 7))
+lookupList = (list(set(lookupList)))
+
+print(visited)
+print(lookupList)
+print(adjList)
+# print((n**k-n-minus) % (10**9 + 7))
 
 # 13 3
 # 9 13 1
